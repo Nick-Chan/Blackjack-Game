429123380fb0e601a2bd27bb3d91463e29b52257
Nick Chan <ncchan16@gmail.com>
2025-01-07 23:57:56 +0000

    Added money and betting

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Configure HttpClient for API calls (if needed)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Run the application
 await builder.Build().RunAsync();
